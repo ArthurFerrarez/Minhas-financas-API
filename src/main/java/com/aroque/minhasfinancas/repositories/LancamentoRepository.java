4721672d6aa226bf406914c613e7f706f7298e0a
ArthurFerrarez <arthurferrarez05@gmail.com>
2022-07-05 13:17:34 -0300

    Efetivando status

--- src/main/java/com/aroque/minhasfinancas/repositories/LancamentoRepository.java
+++ src/main/java/com/aroque/minhasfinancas/repositories/LancamentoRepository.java
@@ -1,5 +1,6 @@
 package com.aroque.minhasfinancas.repositories;
 
+import com.aroque.minhasfinancas.enums.StatusLancamentoEnum;
 import com.aroque.minhasfinancas.enums.TipoLancamentoEnum;
 import com.aroque.minhasfinancas.model.LancamentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +12,11 @@ import java.math.BigDecimal;
 public interface LancamentoRepository extends JpaRepository<LancamentoModel, Long> {
 
     @Query(value = " SELECT sum(l.valor) FROM LancamentoModel l JOIN l.usuario u "
-                 + " WHERE u.id = :idUsuario and l.tipo = :tipo GROUP BY u ")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamentoEnum tipo);
-    // Ele colocou String no tipo, se der errado pode ser aq
+                 + " WHERE u.id = :idUsuario and l.tipo = :tipo and l.status = :status GROUP BY u ")
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
+            @Param("idUsuario") Long idUsuario,
+            @Param("tipo") TipoLancamentoEnum tipo,
+            @Param("status")StatusLancamentoEnum status
+            );
+
 }
